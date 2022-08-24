46d99c06b3ac63ba7d37849629dc2a0c103527b0
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-08-24 23:22:11 +0300

    made a header and popup menu

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import 'virtual:windi.css'
 import './src/sass/main.scss'
+
+const menuSwitcher = document.querySelector('.menu-btn')
+const menu = document.querySelector('.menu')
+
+menuSwitcher.addEventListener('click', () => {
+  // menu.classList.toggle('<md:h-0')
+  menu.classList.toggle('menu--hidden')
+},
+)
