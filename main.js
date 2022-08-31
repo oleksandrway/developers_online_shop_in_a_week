436fa8a49ab35bd176c882e6bc68f18ffebceb2f
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-09-01 02:14:40 +0300

    services section
        adaptability and hover effect

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import 'virtual:windi.css'
 import './src/sass/main.scss'
 
+// menu
+
 const menuSwitcher = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu')
 
@@ -9,3 +11,16 @@ menuSwitcher.addEventListener('click', () => {
   menu.classList.toggle('menu--hidden')
 },
 )
+
+// services cards
+
+const cards = document.querySelectorAll('.services__card')
+
+cards.forEach((arrow) => {
+  arrow.addEventListener('click', () => {
+    const card = arrow.closest('.services__card')
+    card.classList.toggle('services__card--active')
+    card.classList.toggle('services__card--disabled')
+  },
+  )
+})
